0f38d41e889a01dd3e8f950c60044a35d7019528
pedrofernandes <pedrofernandes@guarulhos.sp.gov.br>
2014-02-10 12:34:40 -0200

    Alteração da classe MainActivity, agora o request do JSon está funcionando.

--- gen/com/example/httpandjson/R.java
+++ gen/com/example/httpandjson/R.java
@@ -10,6 +10,11 @@ package com.example.httpandjson;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040001;
+        public static final int gray=0x7f040002;
+        public static final int light_gray=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,34 +22,38 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int padding_large=0x7f050004;
+        public static final int padding_medium=0x7f050003;
+        public static final int padding_small=0x7f050002;
+        public static final int title_size=0x7f050005;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int address_text=0x7f080001;
-        public static final int city_text=0x7f080002;
-        public static final int likes_text=0x7f080004;
-        public static final int name_text=0x7f080000;
-        public static final int phone_text=0x7f080003;
+        public static final int action_settings=0x7f090005;
+        public static final int address_text=0x7f090001;
+        public static final int city_text=0x7f090002;
+        public static final int likes_text=0x7f090004;
+        public static final int name_text=0x7f090000;
+        public static final int phone_text=0x7f090003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int adress_label=0x7f050004;
-        public static final int app_name=0x7f050000;
-        public static final int city_label=0x7f050005;
-        public static final int hello_world=0x7f050002;
-        public static final int likes_label=0x7f050006;
-        public static final int phone_label=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int address_label=0x7f060004;
+        public static final int app_name=0x7f060000;
+        public static final int city_label=0x7f060005;
+        public static final int hello_world=0x7f060002;
+        public static final int likes_label=0x7f060006;
+        public static final int phone_label=0x7f060003;
     }
     public static final class style {
         /** 
@@ -68,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
